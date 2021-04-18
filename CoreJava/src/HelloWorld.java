a7d547d0f6aee13683b6f45b79307be8ecb86e3a
Aakansha Arora <ibmjavafsdmr04@iiht.tech>
2021-04-18 20:01:46 +0530

    HelloWorld example

---
+++ CoreJava/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Hello World");
+		System.out.print("Welcome to ");
+		System.out.printf("World of %s", "java");
+
+	}
+
+}
